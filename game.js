891fa8bad543c71fa8aa092f94771e94ce67865b
JJTank123 <jayla2woods@gmail.com>
2026-03-06 11:15:55 -0600

    Update game.js

--- game.js
+++ game.js
@@ -662,7 +662,7 @@ finishBtn.addEventListener("click", () => {
 
   if (budget >= 0) {
     // ✅ Within budget
-    const code = Math.floor(1000 + Math.random() * 9000); // 4-digit code
+    const code = 200; // 4-digit code
     codeElem.textContent = `Code: ${code}`;
     drawerStatus.textContent = "🔓 Drawer Unlocked!";
     drawer.classList.add("unlocked");
